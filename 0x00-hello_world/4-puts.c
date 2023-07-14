cacaa9c0556f9e69739b91a2f0f7b8ac0dc43eeb
Basma29 <Basma@example.com>
2023-07-13 19:13:20 -0700

    r

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description :A c program that print with puts function 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
  return (0);
