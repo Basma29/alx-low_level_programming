9ff6d03dee27aa08a8e3c8027c1b18a460cd86ee
Basma29 <Basma@example.com>
2023-09-12 13:55:35 -0700

    print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,69 +10,30 @@ void print_to_98(int n)
 int i;
 if (n < 98)
 {
-for (i = n; i <= 98 ; i++)
+for (i = n; i <= 98; i++)
 {
 if (i == 98)
 {
-_putchar(i);
+printf("%d", i);
 break;
 }
-_putchar(i + ',');
-}
-}
-else if (n > 98)
-{
-for (i = n; i >= 98 ; i--)
-{
-if (i == 98)
-{
-_putchar(i);
-break;
-}
-_putchar(i + ',');
+printf("%d", i);
+printf(",");
+printf(" ");
 }
 }
 else
 {
-for (i = 0 ; i <= n ; i++)
+for (i = n; i <= n; i--)
 {
 if (i == 98)
 {
-if (n < 98)
-{
-for (i = n ; i <= 98 ; i++)
-{
-if (i == 98)
-{
-_putchar(i);
+printf("%d", i);
 break;
 }
-_putchar(i + ',');
-}
-}
-else if (n > 98)
-{
-for (i = n ; i >= 98 ; i--)
-{
-if (i == 98)
-{
-_putchar(i);
-break;
-}
-_putchar(i + ',');
-}
-}
-else
-{
-for (i = 0 ; i <= n ; i++)
-{
-if (i == 98)
-{
-_putchar(i);
-break;
-}
-_putchar(i + ',');
-}
+printf("%d", i);
+printf(",");
+printf(" ");
 }
 }
 }
