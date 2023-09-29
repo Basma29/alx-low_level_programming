2cea8eeec913dde06568387039f8ab0914491ea0
Basma29 <Basma@example.com>
2023-09-28 17:27:03 -0700

    new

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,14 +9,13 @@
 */
 int factorial(int n)
 {
-int next;
-if (n < 0)
-return (-1);
-else if (n == 0)
-return (1);
-else
-{
-next = factorial(n - 1);
-return (n * next);
+	int next;
 
+	if (n == 0)
+		return (1);
+	else if (n < 0)
+	return (-1);
+
+	next = factorial(n - 1);
+	return (n * next);
 }
