473a8b653a268546278219f3c48d593d8ccbbf58
Basma29 <Basma@example.com>
2023-10-10 18:27:51 -0700

    main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 #define __FILE__
 /**
- * main - check the code
+ * main - print the name of this source file
  *
  * Return: Always 0.
  */
 
 int main(void)
 {
-printf("%s\n", __FILE__);
-return (0);
+	printf("%s", __FILE__);
+	return (0);
 }
