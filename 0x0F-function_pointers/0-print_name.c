3e542c7576eb75de7dbf4cd84005e757a76b8865
Basma29 <Basma@example.com>
2023-10-13 08:05:32 -0700

    print

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name as is
+ * @f :function which print name
+ * @name: name to print
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL && f == NULL)
+	return;
+	f(name);
+}
